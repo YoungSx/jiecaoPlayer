de76939d5fd5e48da466289f1234952fc9c49355
YoungSx <shangxin@outlook.com>
2017-08-04 01:08:02 +0800

    本地tracker

--- src/components/Client.vue
+++ src/components/Client.vue
@@ -37,16 +37,18 @@ export default {
     addClient () {
       // var torrentId = this.infoHash
       // var torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
-      var torrentId = '08ada5a7a6183aae1e09d831df6748d566095a10'
+      var torrentId = 'c53da4fa28aa2edc1faa91861cce38527414d874'
       client.add(torrentId, {
         announce: [
-          'http://138.68.60.223:8000/announce',
-          'udp://138.68.60.223:8000',
-          'udp://138.68.60.223:8000',
-          'ws://138.68.60.223:8000'
+          'http://localhost:8000/announce',
+          'udp://0.0.0.0:8000',
+          'udp://localhost:8000',
+          'http://localhost:8000/stats',
+          'ws://localhost:8000'
         ]
       }, (torrent) => {
         // Torrents can contain many files. Let's use the .mp4 file
+        console.log(torrent.files)
         var file = torrent.files.find(function (file) {
           return file.name.endsWith('.mp4')
         })
