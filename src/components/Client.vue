15d3a2cd05dc4245a9f6277f0383c7c486d72900
YoungSx <shangxin@outlook.com>
2017-08-07 00:01:23 +0800

    取消一段漏掉的注释 已经可以在nodejs中的seed播放

--- src/components/Client.vue
+++ src/components/Client.vue
@@ -36,16 +36,16 @@ export default {
   methods: {
     addClient () {
       // var torrentId = this.infoHash
-      var torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
-      // var torrentId = 'c53da4fa28aa2edc1faa91861cce38527414d874'
+      // var torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
+      var torrentId = 'c53da4fa28aa2edc1faa91861cce38527414d874'
       client.add(torrentId, {
-        // announce: [
-        //   'http://localhost:8000/announce',
-        //   'udp://0.0.0.0:8000',
-        //   'udp://localhost:8000',
-        //   'http://localhost:8000/stats',
-        //   'ws://localhost:8000'
-        // ]
+        announce: [
+          'http://localhost:8000/announce',
+          'udp://0.0.0.0:8000',
+          'udp://localhost:8000',
+          'http://localhost:8000/stats',
+          'ws://localhost:8000'
+        ]
       }, (torrent) => {
         // Torrents can contain many files. Let's use the .mp4 file
         console.log(torrent.files)
@@ -194,14 +194,15 @@ export default {
         console.error('Unsupported MIME type or codec: ', mimeCodec)
       }
       function sourceOpen (_) {
-        // console.log(this.readyState); // open
+        console.log(this.readyState) // open
         var mediaSource = this
         var sourceBuffer = mediaSource.addSourceBuffer(mimeCodec)
-        fetchAB('videoName', function (buf) {
+        fetchAB('http://138.68.60.223/Sintel.mp4', function (buf) {
           sourceBuffer.addEventListener('updateend', function (_) {
-            // debugger
+            debugger
+            console.log(mediaSource.readyState)
             mediaSource.endOfStream()
-            // video.play()
+            video.play()
             console.log(mediaSource.readyState) // ended
           })
           console.log(buf)
@@ -209,6 +210,15 @@ export default {
         })
       }
       function fetchAB (url, cb) {
+        // console.log(url)
+        // var xhr = new XMLHttpRequest()
+        // xhr.open('get', url)
+        // xhr.responseType = 'arraybuffer'
+        // xhr.onload = function () {
+        //   debugger
+        //   cb(xhr.response)
+        // }
+        // xhr.send()
         cb(_testBuffer)
       }
     }
