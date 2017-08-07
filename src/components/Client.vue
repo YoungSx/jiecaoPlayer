b6231239e3bb589112aa3ce3ff833b838357741f
yangshangxin <yangshangxin@bilibili.com>
2017-08-07 21:32:27 +0800

    添加转码后的mp4hash

--- src/components/Client.vue
+++ src/components/Client.vue
@@ -37,7 +37,8 @@ export default {
     addClient () {
       // var torrentId = this.infoHash
       // var torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
-      var torrentId = 'c53da4fa28aa2edc1faa91861cce38527414d874'
+      // var torrentId = 'c53da4fa28aa2edc1faa91861cce38527414d874'
+      var torrentId = '5f7e8d2d8b322b00e0eeffe78d5d53a92c475a88'
       client.add(torrentId, {
         announce: [
           'http://localhost:8000/announce',
